2696ed5ae870a6fd12c4de9291e5aa10e52960b0
Ekaterina Geletskaya <geleczkak26@gmail.com>
2021-10-02 16:40:08 +0300

    added ex-3

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
 /*
 * Стоврити 2-вимірний масив HEIGHT * WIDTH, заповнити його цілими числами 1, 2, 3 ітд змійкою
-* починаючи з лівого верхнього кута зліва-направо та зверху-вниз,
+* починаючи з правого нижнього кута знизу-верх,
 * наприклад, при HEIGHT = 3, WIDTH = 4
 * [
-*   [1,  2,  3,  4],
-*   [8,  7,  6,  5],
-*   [9,  10, 11, 12]
+*   [10,  9,  4,  3],
+*   [11,  8,  5,  2],
+*   [12,  7,  6,  1]
 * ]
 * */
 
 const array = [];
-const HEIGHT = 5;
+const HEIGHT = 7;
 const WIDTH = 5;
 
 for (let row = 0; row < HEIGHT; row++) {
     array[row] = [];
-    const isOdd = row % 2 === 0;
-    const previousRowsSum = row * WIDTH;
+
     for (let column = 0; column < WIDTH; column++) {
-        if(isOdd) {
-            array[row][column] = column + 1 + previousRowsSum;
+        const isOdd = (WIDTH - column) % 2 == 0;
+        const previousValue = (WIDTH - column - 1) * HEIGHT;
+
+        if(!isOdd) {
+            array[row][column] = previousValue + HEIGHT - row;
         } else {
-            array[row][column] = WIDTH - column + previousRowsSum;
+            array[row][column] = previousValue + row + 1;
         }
     }
 }
 
-console.log(array);
+console.table(array);
