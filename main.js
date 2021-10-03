a017505bd2df2ce2f0b75711450a22af937f4e2d
Ekaterina Geletskaya <geleczkak26@gmail.com>
2021-10-03 13:44:48 +0300

    added ex-2.2

--- main.js
+++ main.js
@@ -1,32 +1,27 @@
-/*
-// Задано масив array розмірністю 9x9 заповнений цифрами [1..9].
-// Перевірити, чи є він "магічним квадратом", тобто в кожному рядку, кожній колонці,
-// та кожному малому квадраті 3x3 не повинні повторюватися цифри.
-* */
+// Створити масив HEIGHT * WIDTH, заповнює його числами, які зростають від країв до центру, починаючи з 1.
+// Наприклад, при HEIGHT = 5, та WIDTH = 8.
+//   [
+//     [1, 1, 1, 1, 1, 1, 1, 1],
+//     [1, 2, 2, 2, 2, 2, 2, 1],
+//     [1, 2, 3, 3, 3, 3, 2, 1],
+//     [1, 2, 2, 2, 2, 2, 2, 1],
+//     [1, 1, 1, 1, 1, 1, 1, 1]
+//   ]
 
-const array = [
-  [2, 1, 5, 3, 7, 9, 8, 6, 4],
-  [9, 8, 6, 1, 2, 4, 3, 5, 7],
-  [7, 3, 4, 8, 5, 6, 2, 1, 9],
-  [4, 5, 2, 7, 8, 1, 6, 9, 3],
-  [8, 6, 9, 5, 4, 3, 1, 7, 2],
-  [3, 7, 1, 6, 9, 2, 4, 8, 5],
-  [5, 2, 7, 4, 1, 8, 9, 3, 6],
-  [6, 4, 8, 9, 3, 7, 5, 2, 1],
-  [1, 9, 3, 2, 6, 5, 7, 4, 8]
-];
+const array = [];
 
-let isMagicSquare = true;
-const arraySize = array.length;
+const HEIGHT = 5;
+const WIDTH = 8;
 
-for (let rowIndex = 0; rowIndex < arraySize; rowIndex++) {
-    const uniqueItems = [];
-    for (let columnIndex = 0; columnIndex < arraySize; columnIndex++) {
-        if(uniqueItems.indexOf(array[rowIndex][columnIndex]) !== -1) {
-            isMagicSquare = false;
-        }
-        uniqueItems.push(array[rowIndex][columnIndex]);
+for (let i = 0; i < HEIGHT; i++) {
+    let row = [];
+    for (let j = 0; j < WIDTH; j++) {
+
+        let widthNum = Math.min(j + 1, WIDTH - 1);
+        let heightNum = Math.min(i + 1, HEIGHT - i);
+        row[j] = Math.min(widthNum, heightNum);
     }
+    array[i] = row;
 }
 
-console.log(isMagicSquare);
+console.table(array);
